1c4bdd2a27b5fb6ba4958c9a2e4263d0e6de2f0f
Justin Paystrup <pay19011@byui.edu>
2022-09-20 15:54:07 -0600

    Created intial dotet project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+
+
+
